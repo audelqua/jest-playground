df37935be983fd5e800fc00c457ce36192cf8a90
audrey <mbozorgi604@gmail.com>
2022-09-15 10:56:32 +0430

    implement express server in react and run it on port 9000 and connect it to react webpack local server on port 3000

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+import express from 'express' //Line 1
 
+
+
+
+const app = express(); //Line 2
+const port = process.env.PORT || 9000; //Line 3
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+interface ResponseObject { 
+
+}
+
+const App: React.FC = () => {
+  const [state, setState] = useState<ResponseObject>()
+  console.log('state', state);
+  
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    console.log('response', response);
+    
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  }
+
+  useEffect(() => {
+    callBackendAPI()
+      .then(res => setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="App">
       
